efe001805cf31cc5ae4e8e34a1cb8833247d32b7
sabatesduran <sabatesduran@gmail.com>
2014-05-16 15:50:52 +0200

    Folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+__author__ = 'Dídac Sabatés'
+from distutils.core import setup
+setup(
+    name='PeachStore',
+    version='1.0',
+    author='Dídac Sabatés',
+    author_email='sabatesduran@gmail.com',
+    description='Simulate an App Store',
+    packager=['PeachStore'],
+    classifiers=''
+)
